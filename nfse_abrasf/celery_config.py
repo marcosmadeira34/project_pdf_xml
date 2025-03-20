0a552c9610305906928051993b735cc666de18e6
Marcos Madeira <mmadeirasilva5@gmail.com>
2025-03-20 11:38:46 -0300

    Adicionando arquivo requirements.txt

--- nfse_abrasf/celery_config.py
+++ nfse_abrasf/celery_config.py
@@ -8,5 +8,8 @@ app = Celery("nfse_abrasf")
 # Pega a configuração do Django
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+# Configurar o broker
+app.conf.broker_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+
 # Descobre tasks automaticamente em apps instalados
 app.autodiscover_tasks()
