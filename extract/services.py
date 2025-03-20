fe9bf0005ababa16c8b8cfedc0d7984ca3d965c5
Marcos Madeira <mmadeirasilva5@gmail.com>
2025-03-19 21:50:36 -0300

    formatando dados corretamente

--- extract/services.py
+++ extract/services.py
@@ -432,10 +432,18 @@ class XMLGenerator:
         # Agora você pode usar a string formatada
         etree.SubElement(valores_nfse, "BaseCalculo").text = base_calculo_formatada
 
-        aliquota = str(dados.get("aliquota", ""))
+        aliquota = str(dados.get("aliquota", "")).strip()
+
         # Remove o caractere '%' e substitui a vírgula por ponto
         aliquota_formatada = aliquota.replace('%', '').replace(',', '.')
-        etree.SubElement(valores_nfse, "Aliquota").text = aliquota_formatada
+
+        # Tenta converter para float, lançando erro se for inválido
+        if aliquota_formatada:
+            aliquota_float = float(aliquota_formatada)
+        else:
+            raise ValueError(f"Valor inválido para alíquota: {aliquota}")
+
+        etree.SubElement(valores_nfse, "Aliquota").text = str(aliquota_float)
 
         valor_iss = str(dados.get("valorIss", ""))
         # Remove o ponto de milhar e substitui a vírgula por ponto
@@ -527,10 +535,18 @@ class XMLGenerator:
         etree.SubElement(valores_servico, "ValorIss").text = valor_iss_servico_formatado
 
         # Aliquota
-        aliquota_servico = str(dados.get("aliquota", "0.00"))
-        aliquota_servico_formatada = aliquota_servico.replace(',', '.').replace(' ', '')  # Remove espaços e converte vírgulas
-        aliquota_servico_formatada = "{:.2f}".format(float(aliquota_servico_formatada))  # Garante 2 casas decimais
-        etree.SubElement(valores_servico, "Aliquota").text = aliquota_servico_formatada
+        aliquota_servico = str(dados.get("aliquota", "")).strip()
+
+        # Remove o caractere '%' e substitui a vírgula por ponto
+        aliquota_formatada = aliquota_servico.replace('%', '').replace(',', '.')
+
+        # Tenta converter para float, lançando erro se for inválido
+        if aliquota_formatada:
+            aliquota_float = float(aliquota_formatada)
+        else:
+            raise ValueError(f"Valor inválido para alíquota: {aliquota}")
+
+        etree.SubElement(valores_nfse, "Aliquota").text = str(aliquota_float)
 
         # IssRetido
         iss_retido = str(dados.get("iss_retido", "0.00"))
