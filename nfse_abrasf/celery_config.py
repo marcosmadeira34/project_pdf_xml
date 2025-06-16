c21daf5419702d0dc6b3041894c6d57ab322005c
Marcos Madeira <mmadeirasilva5@gmail.com>
2025-06-16 17:57:39 -0300

    Fix config Redis to Heroku

--- nfse_abrasf/celery_config.py
+++ nfse_abrasf/celery_config.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+import ssl
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nfse_abrasf.settings")
 
@@ -9,7 +10,13 @@ app = Celery("nfse_abrasf")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Configurar o broker
-app.conf.broker_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+app.conf.broker_url = os.getenv('REDIS_TLS_URL', os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
+
+# Configurar SSL se necessário
+app.conf.broker_use_ssl = {
+    'ssl_cert_reqs': ssl.CERT_NONE
+}
+
 
 # Descobre tasks automaticamente em apps instalados
 app.autodiscover_tasks()
